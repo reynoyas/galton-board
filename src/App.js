a5b92f244cc1c3a75aadc8a04cbdd0ed0ccebcf1
reynoyas <yasilizbeth@gmail.com>
2023-04-19 15:44:14 -0700

    Added TODOs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
           Learn React
       </header>
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Matter from 'matter-js';
+
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Create a Matter.js engine
+
+// Create the Gatlon board as a series of Matter.js bodies
+
+// Create a Matter.js renderer
+
+// Create the player's circle (ball)
+
+// Create the pegs
+
+// Create the left and right walls (bars)
+
+// Start the Matter.js engine and renderer
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
