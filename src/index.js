ef97e9e98c14869654bbf18d3e2bf05e020579dc
reynoyas <yasilizbeth@gmail.com>
2023-04-19 15:59:08 -0700

    Create Matter engine and renderer

--- src/index.js
+++ src/index.js
@@ -4,10 +4,25 @@ import Matter from 'matter-js';
 import reportWebVitals from './reportWebVitals';
 
 // Create a Matter.js engine
+const engine = Matter.Engine.create();
+const world = engine.world;
 
 // Create the Gatlon board as a series of Matter.js bodies
+let Runner = Matter.Runner,
+    Bodies = Matter.Bodies,
+    Composite = Matter.Composite;
 
 // Create a Matter.js renderer
+const render = Matter.Render.create({
+  element: document.body,
+  engine: engine,
+  options: {
+    width: window.innerWidth,
+    height: window.innerHeight,
+    wireframes: false,
+    background: "black"
+  }
+}); 
 
 // Create the player's circle (ball)
 
@@ -16,6 +31,8 @@ import reportWebVitals from './reportWebVitals';
 // Create the left and right walls (bars)
 
 // Start the Matter.js engine and renderer
+Matter.Engine.run(engine);
+Matter.Render.run(render);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
