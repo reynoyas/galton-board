4e4073565f1bd699509b4ba3f2419c2df70c0689
reynoyas <yasilizbeth@gmail.com>
2023-04-20 12:55:05 -0700

    Created pegs and fixed left/right bars

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ let Runner = Matter.Runner,
 
 // Position of x and y axis
 let xPos = 150;
-let yPos = 400;
+let yPos = 140;
 const size = 7;
 
 // Create a Matter.js renderer
@@ -33,19 +33,41 @@ const render = Matter.Render.create({
 Composite.add(world, Bodies.circle(300, 10, size));
 
 // Create the pegs
+const pegs = [];
+const rows = 8;
+for(let i = 0; i<=rows; i++){
+  for(let j=1; j < i; j++){
+    pegs.push(
+      Bodies.circle( //40
+        300 + (j * 40 - i * (40/2)),
+        i * 30,
+        4,
+        {
+          isStatic: true,
+          render:{fillStyle: "white"}
+        })
+    )
+  }
+}
+Composite.add(world, pegs);
 
 // Create the left and right walls (bars)
-for(let i = 0; i<=7; i++){
+for(let i = 0; i<=10; i++){
   Composite.add(world, Bodies.rectangle(xPos += 25, 400, 2, 200, 
     { 
-      isStatic: true
+      isStatic: true,
+      render:{fillStyle: "white"}
     }
   ));
 }
 
 // Ground, floor of board
 // rectangle(x, y, width, height)
-Composite.add(world, Bodies.rectangle(300, 500, 300, 2, { isStatic: true}));
+Composite.add(world, Bodies.rectangle(300, 500, 250, 2, 
+  { isStatic: true,
+    render:{fillStyle: "white"}
+
+  }));
 
 // Counter
 
